fa9a4e61954ba5356fac7a8e8c68354e46f344d9
Jan R <github@gofreerev.com>
2015-01-15 10:42:05 +0100

    refactoring angularJS main/auth page - 3

--- app/assets/javascripts/gofreerev.js
+++ app/assets/javascripts/gofreerev.js
@@ -2969,6 +2969,104 @@ angular.module('gifts', ['ngRoute'])
             });
         $routeProvider.otherwise({redirectTo: '/gifts'});
     })
+    .factory('TextService', ['$sce', function($sce) {
+        console.log('TextService loaded') ;
+
+        var self = this;
+
+        var appname = Gofreerev.rails['APP_NAME'];
+
+        // language specific gift controller constants
+        // todo: client side change language without page refresh?
+        self.init_language_constants = function () {
+            self.language = I18n.locale || I18n.defaultLocale;
+            self.texts = {
+                new_gift: {
+                    header_line: I18n.t('js.new_gift.header_line'),
+                    price_title: I18n.t('js.new_gift.price_title',
+                        {
+                            min_interest: Gofreerev.rails['NEG_INT_POS_BALANCE_PER_YEAR'],
+                            max_interest: Gofreerev.rails['NEG_INT_NEG_BALANCE_PER_YEAR']
+                        }),
+                    price_prompt: I18n.t('js.new_gift.price_prompt'),
+                    price_placeholder: I18n.t('js.new_gift.price_placeholder'),
+                    price_free: I18n.t('js.new_gift.price_free'),
+                    direction_giver_prompt: I18n.t('js.new_gift.direction_giver_prompt'),
+                    direction_receiver_prompt: I18n.t('js.new_gift.direction_receiver_prompt'),
+                    description_title: I18n.t('js.new_gift.description_title'),
+                    description_prompt: I18n.t('js.new_gift.description_prompt'),
+                    description_placeholder: I18n.t('js.new_gift.description_placeholder'),
+                    submit_button_text: I18n.t('js.new_gift.submit_button_text'),
+                    file_prompt: I18n.t('js.new_gift.file_prompt'),
+                    file_placeholder: I18n.t('js.new_gift.file_placeholder'),
+                    link_prompt1: I18n.t('js.new_gift.link_prompt1'),
+                    link_prompt2: I18n.t('js.new_gift.link_prompt2'),
+                    link_placeholder: I18n.t('js.new_gift.link_placeholder')
+                },
+                gifts: {
+                    header: {
+                        giver: I18n.t('js.gifts_header.giver'),
+                        receiver: I18n.t('js.gifts_header.receiver'),
+                        link_and_text: I18n.t('js.gifts_header.link_and_text')
+                    },
+                    link_title: I18n.t('js.gifts.gift_link_title'),
+                    show_full_text: I18n.t('js.gifts.show_full_text'),
+                    like_gift: I18n.t('js.gifts.like_gift'),
+                    unlike_gift: I18n.t('js.gifts.unlike_gift'),
+                    follow_gift: I18n.t('js.gifts.follow_gift'),
+                    unfollow_gift: I18n.t('js.gifts.unfollow_gift'),
+                    delete_gift: I18n.t('js.gifts.delete_gift'),
+                    confirm_delete_gift_1: I18n.t('js.gifts.confirm_delete_gift_1'), // confirm with warning (balance changed)
+                    confirm_delete_gift_2: I18n.t('js.gifts.confirm_delete_gift_2'),  // confirm without warning
+                    hide_gift: I18n.t('js.gifts.hide_gift'),
+                    confirm_hide_gift: I18n.t('js.gifts.confirm_hide_gift'),
+                    no_gifts_was_found_html: $sce.trustAsHtml(I18n.t('js.gifts.no_gifts_was_found_html', {appname: appname})),
+                    no_friends_was_found_html: $sce.trustAsHtml(I18n.t('js.gifts.no_friends_was_found_html', {appname: appname})),
+                    invite_friends_prompt: I18n.t('js.gifts.invite_friends_prompt'),
+                    invite_friends_message_title: I18n.t('js.gifts.invite_friends_message_title', {appname: appname}),
+                    invite_friends_message_body: I18n.t('js.gifts.invite_friends_message_body')
+                },
+                comments: {
+                    comment_text: I18n.t('js.comments.comment_text'),
+                    show_full_text: I18n.t('js.comments.show_full_text'),
+                    cancel_new_deal: I18n.t('js.comments.cancel_new_deal'),
+                    confirm_cancel_new_deal: I18n.t('js.comments.confirm_cancel_new_deal'),
+                    accept_new_deal: I18n.t('js.comments.accept_new_deal'),
+                    confirm_accept_new_deal: I18n.t('js.comments.confirm_accept_new_deal'),
+                    reject_new_deal: I18n.t('js.comments.reject_new_deal'),
+                    confirm_reject_new_deal: I18n.t('js.comments.confirm_reject_new_deal'),
+                    delete_comment: I18n.t('js.comments.delete_comment'),
+                    confirm_delete_comment: I18n.t('js.comments.confirm_delete_comment')
+                },
+                new_comment: {
+                    check_box_prompt: I18n.t('js.new_comment.check_box_prompt'),
+                    comment_placeholder: I18n.t('js.new_comment.comment_placeholder'),
+                    submit_button_text: I18n.t('js.new_comment.submit_button_text')
+                },
+                auth: {
+                    app_login_header: I18n.t('js.auth.app_login_header', {appname: appname}),
+                    app_login_title: I18n.t('js.auth.app_login_title', {appname: appname}),
+                    provider_login_header: I18n.t('js.auth.provider_login_header', {appname: appname}),
+                    provider_login_title: I18n.t('js.auth.provider_login_title', {appname: appname}),
+                    password_prompt: I18n.t('js.auth.password_prompt'),
+                    password_placeholder: I18n.t('js.auth.password_placeholder', {appname: appname}),
+                    log_in_button: I18n.t('js.auth.log_in_button'),
+                    confirm_password_prompt: $sce.trustAsHtml(I18n.t('js.auth.confirm_password_prompt')),
+                    confirm_password_placeholder: I18n.t('js.auth.confirm_password_placeholder', {appname: appname}),
+                    register_button: I18n.t('js.auth.register_button'),
+                    radio_log_in_prompt: I18n.t('js.auth.radio_log_in_prompt'),
+                    radio_register_prompt: I18n.t('js.auth.radio_register_prompt')
+                }
+            };
+        }
+        self.init_language_constants();
+
+        return {
+            language: self.language,
+            texts: self.texts,
+            init_language_constants: self.init_language_constants
+        }
+    }])
     .factory('UserService', ['$window', function($window) {
         console.log('UserService loaded') ;
 
@@ -3152,10 +3250,11 @@ angular.module('gifts', ['ngRoute'])
         var self = this ;
         self.userService = userService ;
     }])
-    .controller('AuthCtrl', ['UserService', '$window', function(userService, $window) {
+    .controller('AuthCtrl', ['TextService', 'UserService', '$window', function(textService, userService, $window) {
         console.log('AuthCtrl loaded') ;
         var self = this ;
         self.userService = userService ;
+        self.texts = textService.texts ;
         self.providers = [] ;
         // self.providers.push({provider: 'gofreerev', name: 'Gofreerev'}) ;
         var api_names = Gofreerev.rails['API_CAMELIZE_NAME'] ;
@@ -3166,10 +3265,6 @@ angular.module('gifts', ['ngRoute'])
         }
         self.is_gofreerev = function (provider) {
             return (provider.provider == 'gofreerev')
-        };
-        self.show_device_password = function() {
-            if (userService.is_logged_in_with_device()) return false ;
-            return true ;
         };
 
         // radio group login or register?
@@ -3226,82 +3321,14 @@ angular.module('gifts', ['ngRoute'])
             }
         }
     }])
-    .controller('GiftsCtrl', ['$location', '$http', '$document', '$window', '$sce', 'UserService', function ($location, $http, $document, $window, $sce, userService) {
+    .controller('GiftsCtrl', ['$location', '$http', '$document', '$window', '$sce', 'UserService', 'TextService', function ($location, $http, $document, $window, $sce, userService, textService) {
         console.log('GiftsCtrl loaded') ;
         var self = this;
 
+        self.texts = textService.texts ;
+
         var appname = Gofreerev.rails['APP_NAME'];
 
-        // language specific gift controller constants
-        // todo: client side change language without page refresh?
-        function init_language_constants() {
-            self.language = I18n.locale || I18n.defaultLocale;
-            self.texts = {
-                new_gift: {
-                    header_line: I18n.t('js.new_gift.header_line'),
-                    price_title: I18n.t('js.new_gift.price_title',
-                        {
-                            min_interest: Gofreerev.rails['NEG_INT_POS_BALANCE_PER_YEAR'],
-                            max_interest: Gofreerev.rails['NEG_INT_NEG_BALANCE_PER_YEAR']
-                        }),
-                    price_prompt: I18n.t('js.new_gift.price_prompt'),
-                    price_placeholder: I18n.t('js.new_gift.price_placeholder'),
-                    price_free: I18n.t('js.new_gift.price_free'),
-                    direction_giver_prompt: I18n.t('js.new_gift.direction_giver_prompt'),
-                    direction_receiver_prompt: I18n.t('js.new_gift.direction_receiver_prompt'),
-                    description_title: I18n.t('js.new_gift.description_title'),
-                    description_prompt: I18n.t('js.new_gift.description_prompt'),
-                    description_placeholder: I18n.t('js.new_gift.description_placeholder'),
-                    submit_button_text: I18n.t('js.new_gift.submit_button_text'),
-                    file_prompt: I18n.t('js.new_gift.file_prompt'),
-                    file_placeholder: I18n.t('js.new_gift.file_placeholder'),
-                    link_prompt1: I18n.t('js.new_gift.link_prompt1'),
-                    link_prompt2: I18n.t('js.new_gift.link_prompt2'),
-                    link_placeholder: I18n.t('js.new_gift.link_placeholder')
-                },
-                gifts: {
-                    header: {
-                        giver: I18n.t('js.gifts_header.giver'),
-                        receiver: I18n.t('js.gifts_header.receiver'),
-                        link_and_text: I18n.t('js.gifts_header.link_and_text')
-                    },
-                    link_title: I18n.t('js.gifts.gift_link_title'),
-                    show_full_text: I18n.t('js.gifts.show_full_text'),
-                    like_gift: I18n.t('js.gifts.like_gift'),
-                    unlike_gift: I18n.t('js.gifts.unlike_gift'),
-                    follow_gift: I18n.t('js.gifts.follow_gift'),
-                    unfollow_gift: I18n.t('js.gifts.unfollow_gift'),
-                    delete_gift: I18n.t('js.gifts.delete_gift'),
-                    confirm_delete_gift_1: I18n.t('js.gifts.confirm_delete_gift_1'), // confirm with warning (balance changed)
-                    confirm_delete_gift_2: I18n.t('js.gifts.confirm_delete_gift_2'),  // confirm without warning
-                    hide_gift: I18n.t('js.gifts.hide_gift'),
-                    confirm_hide_gift: I18n.t('js.gifts.confirm_hide_gift'),
-                    no_gifts_was_found_html: $sce.trustAsHtml(I18n.t('js.gifts.no_gifts_was_found_html', {appname: appname})),
-                    no_friends_was_found_html: $sce.trustAsHtml(I18n.t('js.gifts.no_friends_was_found_html', {appname: appname})),
-                    invite_friends_prompt: I18n.t('js.gifts.invite_friends_prompt'),
-                    invite_friends_message_title: I18n.t('js.gifts.invite_friends_message_title', {appname: appname}),
-                    invite_friends_message_body: I18n.t('js.gifts.invite_friends_message_body')
-                },
-                comments: {
-                    comment_text: I18n.t('js.comments.comment_text'),
-                    show_full_text: I18n.t('js.comments.show_full_text'),
-                    cancel_new_deal: I18n.t('js.comments.cancel_new_deal'),
-                    confirm_cancel_new_deal: I18n.t('js.comments.confirm_cancel_new_deal'),
-                    accept_new_deal: I18n.t('js.comments.accept_new_deal'),
-                    confirm_accept_new_deal: I18n.t('js.comments.confirm_accept_new_deal'),
-                    reject_new_deal: I18n.t('js.comments.reject_new_deal'),
-                    confirm_reject_new_deal: I18n.t('js.comments.confirm_reject_new_deal'),
-                    delete_comment: I18n.t('js.comments.delete_comment'),
-                    confirm_delete_comment: I18n.t('js.comments.confirm_delete_comment')
-                },
-                new_comment: {
-                    check_box_prompt: I18n.t('js.new_comment.check_box_prompt'),
-                    comment_placeholder: I18n.t('js.new_comment.comment_placeholder'),
-                    submit_button_text: I18n.t('js.new_comment.submit_button_text')
-                }
-            };
-        }
-        init_language_constants();
         self.default_no_comments = 3 ;
 
         self.is_logged_in = function () {
